5d5c805106f3b206b073a6121257ddec737a0c6f
Ravinder Kumar <ravinder.kumar.apj@gmail.com>
2017-03-29 17:23:01 +0530

    Refactos: js code for enabled payemtn gateways core setting field

--- assets/js/admin/admin-settings.js
+++ assets/js/admin/admin-settings.js
@@ -14,44 +14,49 @@ jQuery(document).ready(function ($) {
 	/**
 	 * Set payment gateway list under default payment gaetway option
 	 */
-	var $payment_gateways = jQuery( 'ul.give-payment-gatways-list' ),
+	var $payment_gateways = jQuery( 'ul.give-checklist-fields', 'td.give-forminp-enabled_gateways' ),
 		$payment_gateway_list_item = $('input', $payment_gateways),
 		$default_gateway           = $('#default_gateway');
 
+
+	if( $payment_gateways.length ) {
 		$payment_gateway_list_item.on('click', function () {
 
-		// Bailout.
-		if( $(this)[0].hasAttribute( 'readonly' ) ) {
-			return false;
-		}
-
-		// Selectors.
-		var saved_default_gateway      = $default_gateway.val(),
-			active_payment_option_html = '',
-			$active_payment_gateways   = $('input:checked', $payment_gateways);
-
-		// Set last active payment gateways to readonly.
-		if( 1 === $active_payment_gateways.length ){
-			$active_payment_gateways.prop( 'readonly', true );
-		}else{
-			$('input[readonly]:checked', $payment_gateways ).removeAttr('readonly');
-		}
-
-		// Create option html from active payment gateway list.
-		$active_payment_gateways.each(function (index, item) {
-			item           = $(item);
-			var item_value = item.attr('name').match(/\[(.*?)\]/)[1];
-
-			active_payment_option_html += '<option value="' + item_value + '"';
-
-			if (saved_default_gateway === item_value) {
-				active_payment_option_html += ' selected="selected"';
+			// Bailout.
+			if( $(this)[0].hasAttribute( 'readonly' ) ) {
+				return false;
 			}
 
-			active_payment_option_html += '>' + item.next('label').text() + '</option>';
-		});
+			// Selectors.
+			var saved_default_gateway      = $default_gateway.val(),
+				active_payment_option_html = '',
+				$active_payment_gateways   = $('input:checked', $payment_gateways);
 
-		// Update select html.
-		$default_gateway.html(active_payment_option_html);
-	});
+			// Set last active payment gateways to readonly.
+			if( 1 === $active_payment_gateways.length ){
+				$active_payment_gateways.prop( 'readonly', true );
+			}else{
+				$('input[readonly]:checked', $payment_gateways ).removeAttr('readonly');
+			}
+
+			// Create option html from active payment gateway list.
+			$active_payment_gateways.each(function (index, item) {
+				item           = $(item);
+				var item_value = item.attr('name').match(/\[(.*?)\]/)[1];
+
+				active_payment_option_html += '<option value="' + item_value + '"';
+
+				if (saved_default_gateway === item_value) {
+					active_payment_option_html += ' selected="selected"';
+				}
+
+				active_payment_option_html += '>' + item.parent('label').text() + '</option>';
+			});
+
+			console.log($active_payment_gateways);
+
+			// Update select html.
+			$default_gateway.html(active_payment_option_html);
+		});
+	}
 });
