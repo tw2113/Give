93788db9f40d6adcdee3438aad17081382a454b1
Devin Walker <sdtucson@gmail.com>
2017-04-20 15:34:30 -0700

    Revamped Give_Sessions to store in new table give_sessions rather than utilizing PHP sessions.

--- includes/class-give-session.php
+++ includes/class-give-session.php
@@ -17,379 +17,187 @@ if ( ! defined( 'ABSPATH' ) ) {
 /**
  * Give_Session Class
  *
- * This is a wrapper class for WP_Session / PHP $_SESSION and handles the storage of Give sessions.
- *
  * @since 1.0
  */
 class Give_Session {
 
 	/**
-	 * Holds our session data
-	 *
-	 * @since  1.0
-	 * @access private
-	 *
-	 * @var    WP_Session/array
+	 * @var mixed|null
 	 */
-	private $session;
+	public $wpdb = null;
 
 	/**
-	 * Whether to use PHP $_SESSION or WP_Session
-	 *
-	 * @since  1.0
-	 * @access private
-	 *
-	 * @var    bool
+	 * @var string
 	 */
-	private $use_php_sessions = false;
+	public $table;
 
 	/**
-	 * Expiration Time
-	 *
-	 * @since  1.0
-	 * @access private
-	 *
-	 * @var    int
+	 * @var int
 	 */
-	private $exp_option = false;
+	public $version = 1;
 
 	/**
-	 * Session index prefix
-	 *
-	 * @since  1.0
-	 * @access private
-	 *
-	 * @var    string
-	 */
-	private $prefix = '';
-
-	/**
-	 * Class Constructor
-	 *
-	 * Defines our session constants, includes the necessary libraries and retrieves the session instance.
-	 *
-	 * @since  1.0
-	 * @access public
+	 * Give_Session constructor.
 	 */
 	public function __construct() {
 
-		$this->use_php_sessions = $this->use_php_sessions();
-		$this->exp_option       = give_get_option( 'session_lifetime' );
+		$this->wpdb = clone $GLOBALS['wpdb'];
 
-		// PHP Sessions.
-		if ( $this->use_php_sessions ) {
+		$this->table = $this->wpdb->prefix . 'give_sessions';
 
-			if ( is_multisite() ) {
+		$this->maybe_upgrade();
+		session_set_save_handler(
+			array( $this, 'open' ),
+			array( $this, 'close' ),
+			array( $this, 'read' ),
+			array( $this, 'write' ),
+			array( $this, 'destroy' ),
+			array( $this, 'gc' )
+		);
+		register_shutdown_function( 'session_write_close' );
 
-				$this->prefix = '_' . get_current_blog_id();
+		// Garbage cleanup
+		if ( ! wp_next_scheduled( 'give_wpdb_sessions_gc' ) ) {
+			wp_schedule_event( time(), 'hourly', 'give_wpdb_sessions_gc' );
+		}
+		add_action( 'give_wpdb_sessions_gc', array( $this, 'cron_gc' ) );
 
-			}
+	}
 
-			add_action( 'init', array( $this, 'maybe_start_session' ), - 2 );
+	/**
+	 * Open a session.
+	 */
+	public function open() {
+		return true;
+	}
 
-		} else {
+	/**
+	 * Close a session.
+	 */
+	public function close() {
+		return true;
+	}
 
-			if ( ! $this->should_start_session() ) {
-				return;
-			}
+	/**
+	 * Alias for reading session data.
+	 *
+	 * @param string $id Session id.
+	 *
+	 * @return mixed Session data or null.
+	 */
+	public function get( $id ) {
+		return $this->read( $id );
+	}
 
-			// Use WP_Session.
-			if ( ! defined( 'WP_SESSION_COOKIE' ) ) {
-				define( 'WP_SESSION_COOKIE', 'give_wp_session' );
-			}
-
-			if ( ! class_exists( 'Recursive_ArrayAccess' ) ) {
-				require_once GIVE_PLUGIN_DIR . 'includes/libraries/sessions/class-recursive-arrayaccess.php';
-			}
-
-			if ( ! class_exists( 'WP_Session' ) ) {
-				require_once GIVE_PLUGIN_DIR . 'includes/libraries/sessions/class-wp-session.php';
-				require_once GIVE_PLUGIN_DIR . 'includes/libraries/sessions/wp-session.php';
-			}
-
-			add_filter( 'wp_session_expiration_variant', array( $this, 'set_expiration_variant_time' ), 99999 );
-			add_filter( 'wp_session_expiration', array( $this, 'set_expiration_time' ), 99999 );
-
+	/**
+	 * Read session data.
+	 *
+	 * @param string $id Session id.
+	 *
+	 * @return mixed Session data or null.
+	 */
+	public function read( $id ) {
+		if ( ! $this->wpdb ) {
+			return null;
 		}
 
-		// Init Session.
-		if ( empty( $this->session ) && ! $this->use_php_sessions ) {
-			add_action( 'plugins_loaded', array( $this, 'init' ), - 1 );
-		} else {
-			add_action( 'init', array( $this, 'init' ), - 1 );
+		$data = @$this->wpdb->get_var( $this->wpdb->prepare( "SELECT `data` FROM `{$this->table}` WHERE `id` = %s;", $id ) );
+
+		return maybe_unserialize( $data );
+	}
+
+	/**
+	 * Alias for write method.
+	 *
+	 * @param string $id Session id.
+	 * @param string $data Session data (serialized for session storage).
+	 *
+	 * @return null
+	 */
+	public function set( $id, $data ) {
+		return $this->write( $id, $data );
+	}
+
+	/**
+	 * Write a session.
+	 *
+	 * @param string $id Session id.
+	 * @param string $data Session data (serialized for session storage).
+	 *
+	 * @return null
+	 */
+	public function write( $id, $data ) {
+		if ( ! $this->wpdb ) {
+			return null;
 		}
 
-		// Set cookie on Donation Completion page.
-		add_action( 'give_pre_process_donation', array( $this, 'set_session_cookies' ) );
-
+		return $this->wpdb->query( $this->wpdb->prepare( "REPLACE INTO `{$this->table}` VALUES ( %s, %s, %d );", $id, maybe_serialize( $data ), time() ) );
 	}
 
 	/**
-	 * Session Init
+	 * Destroy a session.
 	 *
-	 * Setup the Session instance.
-	 *
-	 * @since  1.0
-	 * @access public
-	 *
-	 * @return array Session instance
-	 */
-	public function init() {
-
-		if ( $this->use_php_sessions ) {
-			$this->session = isset( $_SESSION[ 'give' . $this->prefix ] ) && is_array( $_SESSION[ 'give' . $this->prefix ] ) ? $_SESSION[ 'give' . $this->prefix ] : array();
-		} else {
-			$this->session = WP_Session::get_instance();
-		}
-
-		return $this->session;
-
-	}
-
-	/**
-	 * Get Session ID
-	 *
-	 * Retrieve session ID.
-	 *
-	 * @since  1.0
-	 * @access public
-	 *
-	 * @return string Session ID.
-	 */
-	public function get_id() {
-		return $this->session->session_id;
-	}
-
-	/**
-	 * Get Session
-	 *
-	 * Retrieve session variable for a given session key.
-	 *
-	 * @since  1.0
-	 * @access public
-	 *
-	 * @param  string $key Session key.
-	 *
-	 * @return string      Session variable.
-	 */
-	public function get( $key ) {
-		$key = sanitize_key( $key );
-
-		return isset( $this->session[ $key ] ) ? maybe_unserialize( $this->session[ $key ] ) : false;
-
-	}
-
-	/**
-	 * Set Session
-	 *
-	 * Create a new session.
-	 *
-	 * @since  1.0
-	 * @access public
-	 *
-	 * @param  string $key   Session key.
-	 * @param  string $value Session variable.
-	 *
-	 * @return string        Session variable.
-	 */
-	public function set( $key, $value ) {
-
-		$key = sanitize_key( $key );
-
-		if ( is_array( $value ) ) {
-			$this->session[ $key ] = serialize( $value );
-		} else {
-			$this->session[ $key ] = $value;
-		}
-
-		if ( $this->use_php_sessions ) {
-			$_SESSION[ 'give' . $this->prefix ] = $this->session;
-		}
-
-		return $this->session[ $key ];
-	}
-
-	/**
-	 * Set Session Cookies
-	 *
-	 * Cookies are used to increase the session lifetime using the give setting. This is helpful for when a user closes their browser after making a donation and comes back to the site.
-	 *
-	 * @since  1.4
-	 * @access public
-	 *
-	 * @hook
-	 */
-	public function set_session_cookies() {
-		if ( ! headers_sent() ) {
-			$lifetime = current_time( 'timestamp' ) + $this->set_expiration_time();
-			@setcookie( session_name(), session_id(), $lifetime, COOKIEPATH, COOKIE_DOMAIN, false );
-			@setcookie( session_name() . '_expiration', $lifetime, $lifetime, COOKIEPATH, COOKIE_DOMAIN, false );
-		}
-	}
-
-	/**
-	 * Set Cookie Variant Time
-	 *
-	 * Force the cookie expiration variant time to custom expiration option, less and hour. defaults to 23 hours (set_expiration_variant_time used in WP_Session).
-	 *
-	 * @since  1.0
-	 * @access public
-	 *
-	 * @return int
-	 */
-	public function set_expiration_variant_time() {
-
-		return ( ! empty( $this->exp_option ) ? ( intval( $this->exp_option ) - 3600 ) : 30 * 60 * 23 );
-	}
-
-	/**
-	 * Set Cookie Expiration
-	 *
-	 * Force the cookie expiration time if set, default to 24 hours.
-	 *
-	 * @since  1.0
-	 * @access public
-	 *
-	 * @return int
-	 */
-	public function set_expiration_time() {
-
-		return ( ! empty( $this->exp_option ) ? intval( $this->exp_option ) : 30 * 60 * 24 );
-	}
-
-	/**
-	 * Starts a new session if one has not started yet.
-	 *
-	 * Checks to see if the server supports PHP sessions or if the GIVE_USE_PHP_SESSIONS constant is defined.
-	 *
-	 * @since  1.0
-	 * @access public
-	 *
-	 * @return bool $ret True if we are using PHP sessions, false otherwise.
-	 */
-	public function use_php_sessions() {
-
-		$ret = false;
-
-		// If the database variable is already set, no need to run auto detection.
-		$give_use_php_sessions = (bool) get_option( 'give_use_php_sessions' );
-
-		if ( ! $give_use_php_sessions ) {
-
-			// Attempt to detect if the server supports PHP sessions.
-			if ( function_exists( 'session_start' ) && ! ini_get( 'safe_mode' ) ) {
-
-				$this->set( 'give_use_php_sessions', 1 );
-
-				if ( $this->get( 'give_use_php_sessions' ) ) {
-
-					$ret = true;
-
-					// Set the database option.
-					update_option( 'give_use_php_sessions', true );
-
-				}
-			}
-		} else {
-
-			$ret = $give_use_php_sessions;
-		}
-
-		// Enable or disable PHP Sessions based on the GIVE_USE_PHP_SESSIONS constant.
-		if ( defined( 'GIVE_USE_PHP_SESSIONS' ) && GIVE_USE_PHP_SESSIONS ) {
-			$ret = true;
-		} elseif ( defined( 'GIVE_USE_PHP_SESSIONS' ) && ! GIVE_USE_PHP_SESSIONS ) {
-			$ret = false;
-		}
-
-		return (bool) apply_filters( 'give_use_php_sessions', $ret );
-	}
-
-	/**
-	 * Should Start Session
-	 *
-	 * Determines if we should start sessions.
-	 *
-	 * @since  1.4
-	 * @access public
+	 * @param string $id Session id.
 	 *
 	 * @return bool
 	 */
-	public function should_start_session() {
-
-		$start_session = true;
-
-		if ( ! empty( $_SERVER['REQUEST_URI'] ) ) {
-
-			$blacklist = apply_filters( 'give_session_start_uri_blacklist', array(
-				'feed',
-				'feed',
-				'feed/rss',
-				'feed/rss2',
-				'feed/rdf',
-				'feed/atom',
-				'comments/feed/',
-			) );
-			$uri       = ltrim( $_SERVER['REQUEST_URI'], '/' );
-			$uri       = untrailingslashit( $uri );
-			if ( in_array( $uri, $blacklist ) ) {
-				$start_session = false;
-			}
-			if ( false !== strpos( $uri, 'feed=' ) ) {
-				$start_session = false;
-			}
-			if ( is_admin() ) {
-				$start_session = false;
-			}
+	public function destroy( $id ) {
+		if ( ! $this->wpdb ) {
+			return false;
 		}
 
-		return apply_filters( 'give_start_session', $start_session );
+		return (bool) $this->wpdb->query( $this->wpdb->prepare( "DELETE FROM `{$this->table}` WHERE `id` = %s;", $id ) );
 	}
 
 	/**
-	 * Maybe Start Session
+	 * Garbage collection.
 	 *
-	 * Starts a new session if one hasn't started yet.
+	 * @param $max
 	 *
-	 * @access public
-	 *
-	 * @see    http://php.net/manual/en/function.session-set-cookie-params.php
-	 *
-	 * @return void
+	 * @return bool
 	 */
-	public function maybe_start_session() {
-
-		if ( ! $this->should_start_session() ) {
-			return;
-		}
-
-		if ( ! session_id() && ! headers_sent() ) {
-			session_start();
-		}
-
+	public function gc( $max ) {
+		return true;
 	}
 
 	/**
-	 * Get Session Expiration
-	 *
-	 * Looks at the session cookies and returns the expiration date for this session if applicable
-	 *
-	 * @access public
-	 *
-	 * @return string Formatted expiration date string.
+	 * Compare versions and maybe run an upgrade routine.
 	 */
-	public function get_session_expiration() {
+	public function maybe_upgrade() {
 
-		$expiration = false;
+		$current_version = (int) get_site_option( 'give_wpdb_sessions_version', 0 );
 
-		if ( session_id() && isset( $_COOKIE[ session_name() . '_expiration' ] ) ) {
-
-			$expiration = date( 'D, d M Y h:i:s', intval( $_COOKIE[ session_name() . '_expiration' ] ) );
-
+		if ( version_compare( $this->version, $current_version, '>' ) ) {
+			$this->do_upgrade( $current_version );
 		}
-
-		return $expiration;
-
 	}
+
+	/**
+	 * Perform an upgrade routine.
+	 *
+	 * @param int $current_version The version number from which to perform the upgrades.
+	 */
+	public function do_upgrade( $current_version ) {
+		global $wpdb;
+		if ( $current_version < 1 ) {
+			$wpdb->query( "CREATE TABLE `{$this->table}` (
+				`id` varchar(255) NOT NULL,
+				`data` mediumtext NOT NULL,
+				`timestamp` int(255) NOT NULL,
+				PRIMARY KEY (`id`)
+			) CHARACTER SET utf8 COLLATE utf8_general_ci;" );
+			$current_version = 1;
+			update_site_option( 'give_wpdb_sessions_version', $current_version );
+		}
+	}
+
+	/**
+	 * Cron-powered garbage collection.
+	 */
+	public function cron_gc() {
+		global $wpdb;
+		$wpdb->query( $wpdb->prepare( "DELETE FROM `{$this->table}` WHERE `timestamp` < %d;", time() - HOUR_IN_SECONDS * 24 ) );
+	}
+
 
 }
