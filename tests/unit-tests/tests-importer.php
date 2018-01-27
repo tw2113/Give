32efa482229835cce14e10b11b47ff4e3508edd1
Michael Beckwith <tw2113@gmail.com>
2018-01-27 14:04:41 -0600

    start up an importers test file

---
+++ tests/unit-tests/tests-importer.php
@@ -0,0 +1,23 @@
+<?php
+
+
+/**
+ * @group give_importers
+ */
+class Tests_Importer extends Give_Unit_Test_Case {
+	protected $_object = null;
+
+	/**
+	 * Set it Up
+	 */
+	public function setUp() {
+		parent::setUp();
+	}
+
+	/**
+	 * Tear it Down
+	 */
+	public function tearDown() {
+		parent::tearDown();
+	}
+}
